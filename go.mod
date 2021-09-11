acfd5dd4dfd12153794234fa782a042480fcc37f
calvinwijaya8 <90295805+calvinwijaya8@users.noreply.github.com>
2021-09-12 01:01:25 +0700

    add posgtgres

--- go.mod
+++ go.mod
@@ -13,8 +13,20 @@ require (
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/iancoleman/strcase v0.0.0-20191112232945-16388991a334 // indirect
+	github.com/jackc/pgx/v4 v4.13.0
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20210909211513-a8c4777a87af // indirect
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 )
--- service/main.go
+++ service/main.go
@@ -7,32 +7,67 @@ import (
 	"net"
 
 	pb "github.com/crshao/grpc-graphql-gateway/student"
+	"github.com/jackc/pgx/v4"
 	"google.golang.org/grpc"
 )
 
+func NewStudentManagementServer() *Server {
+	return &Server{}
+}
+
 type Server struct {
+	conn *pgx.Conn
 	pb.UnimplementedStudentManagementServer
+}
+
+func (server *Server) Run() error {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterStudentManagementServer(s, server)
+	log.Printf("Server listening at %v", lis.Addr())
+
+	return s.Serve(lis)
 }
 
 func (s *Server) GetStudents(ctx context.Context, req *pb.GetStudentsParams) (*pb.StudentsList, error) {
 	var students_list *pb.StudentsList = &pb.StudentsList{}
 
-	student := pb.Student{Name: "test", Nim: "123123"}
-	students_list.Students = append(students_list.Students, &student)
+	rows, err := s.conn.Query(context.Background(), "SELECT * FROM students")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		student := pb.Student{}
+		err = rows.Scan(&student.Id, &student.Name, &student.Nim)
+		if err != nil {
+			return nil, err
+		}
+		students_list.Students = append(students_list.Students, &student)
+	}
 
 	return students_list, nil
 }
 
 func main() {
-	conn, err := net.Listen("tcp", ":50051")
+
+	database_url := "postgres://calvin.wijaya:calvin.wijaya@localhost:5432/grpc_graphql_gateway"
+	conn, err := pgx.Connect(context.Background(), database_url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Unable to establish connection: %v", err)
 	}
-	defer conn.Close()
 
-	server := grpc.NewServer()
+	defer conn.Close(context.Background())
 
-	pb.RegisterStudentManagementServer(server, &Server{})
-	log.Printf("Server listening at %v", conn.Addr())
-	server.Serve(conn)
+	var student_management_server *Server = NewStudentManagementServer()
+	student_management_server.conn = conn
+
+	if err := student_management_server.Run(); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
